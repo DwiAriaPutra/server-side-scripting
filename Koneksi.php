f59fd4be46a5ec39ecb3f1ae5526de0f4e20db6a
AriaPutra <dwiariaputra21@gmail.com>
2026-02-27 17:54:37 +0700

    first commit

---
+++ Koneksi.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost';
+$dbname = 'de_projek';
+$username = 'root';
+$password = '';
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname",
+        $username,
+        $password
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi Database Gagal: " . $e->getMessage());
+}
